f17d68eaa43876c19fed129080e350d88a52b38e
Renate <lhopnetla@gmail.com>
2016-11-22 20:10:13 +0100

    zweites recipe

--- js/modules/ASSETGENERATOR.ASSET.display.js
+++ js/modules/ASSETGENERATOR.ASSET.display.js
@@ -45,6 +45,49 @@ ASSETGENERATOR.ASSET.display = (function() {
 		context.fillRect(11*mod,7*mod,2*mod,1*mod);
 		
 	}
+	function chooseNose(bright, dark){
+		context.fillStyle = bright;
+		context.fillRect(8*mod, 7*mod, 2*mod, 4*mod);
+		context.fillStyle = dark;
+		
+		
+		var noses =	[
+			function(){
+			context.fillRect(8*mod, 7*mod, 1*mod, 4*mod);
+			context.fillRect(7*mod, 9*mod, 1*mod, 2*mod);
+				
+			},
+			function(){
+			context.fillRect(8*mod, 7*mod, 1*mod, 1*mod);
+			context.fillRect(7*mod, 9*mod, 2*mod, 3*mod);
+				
+			}		
+		];
+		var index = Math.floor(Math.random() * noses.length); 
+		noses[index]();
+		
+	}
+	function getColoredBlocks(startX, startY,width, height, color){
+		var rgb;
+		var hexColor;
+		var mask = new Array(width).fill(new Array(height).fill(false));
+		for(var y = startY; y<= height;y++){
+			for(var x = startX; x<= width; x++){
+				rgb = context.getImageData(x*mod, y*mod, 1, 1).data;
+				hexColor = "#"+ ("000000" + rgbToHex(rgb[0], rgb[1], rgb[2])).slice(-6);
+				if(hexColor == color){
+					img[x][y] = true;
+				}
+				
+			}
+		}
+		return mask;
+	}
+	function rgbToHex(r, g, b) {
+   		if (r > 255 || g > 255 || b > 255)
+        	throw "Invalid color component";
+    	return ((r << 16) | (g << 8) | b).toString(16);
+	}
 
 	function drawBlock(){
 
